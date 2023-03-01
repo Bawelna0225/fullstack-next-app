874f1e5b532f5b1cd5000466654f7998366000fb
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-03-01 13:03:48 +0100

    yeeting user out of a page if not logged in

--- pages/createPost.jsx
+++ pages/createPost.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Link from 'next/link'
+import { useSession } from 'next-auth/react'
+import Router from 'next/router'
+
 
 export default function createPost() {
 	const [postInfo, setPostInfo] = useState({ title: '', content: '' })
@@ -7,28 +10,38 @@ export default function createPost() {
 	const handleSubmit = async (e) => {
 		e.preventDefault()
 	}
-	return (
-		<div className="create-post">
-			<form onSubmit={handleSubmit}>
-				<h1>Create Post</h1>
-				<div className="input">
-					<span className="title"></span>
-					<label htmlFor="">Title</label>
-					<input value={postInfo.title} onChange={({ target }) => setPostInfo({ ...postInfo, title: target.value })} type="text" required />
-				</div>
-				<div className="input">
-					<span className="content"></span>
-					<label htmlFor="">Content</label>
-					<textarea value={postInfo.content} onChange={({ target }) => setPostInfo({ ...postInfo, content: target.value })} required />
-				</div>
-				<div className="buttons">
-					<small>
-						<Link href="/">Changed your mind?</Link>
-					</small>
+	const { status, data } = useSession()
 
-					<button className="button" type="submit"><span>Create Post</span></button>
-				</div>
-			</form>
-		</div>
-	)
+	useEffect(() => {
+		if (status === 'unauthenticated') Router.replace('/auth/signin')
+	}, [status])
+
+	if (status === 'authenticated') {
+		return (
+			<div className="create-post">
+				<form onSubmit={handleSubmit}>
+					<h1>Create Post</h1>
+					<div className="input">
+						<span className="title"></span>
+						<label htmlFor="">Title</label>
+						<input value={postInfo.title} onChange={({ target }) => setPostInfo({ ...postInfo, title: target.value })} type="text" required />
+					</div>
+					<div className="input">
+						<span className="content"></span>
+						<label htmlFor="">Content</label>
+						<textarea value={postInfo.content} onChange={({ target }) => setPostInfo({ ...postInfo, content: target.value })} required />
+					</div>
+					<div className="buttons">
+						<small>
+							<Link href="/">Changed your mind?</Link>
+						</small>
+
+						<button className="button" type="submit">
+							<span>Create Post</span>
+						</button>
+					</div>
+				</form>
+			</div>
+		)
+	}
 }
