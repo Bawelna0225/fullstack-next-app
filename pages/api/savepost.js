6a6b893aab942623e21f365875c8a140beab4b0c
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-03-02 11:39:36 +0100

    finding user id in database

--- pages/api/savepost.js
+++ pages/api/savepost.js
@@ -3,8 +3,20 @@ import connection from '../../utils/db'
 export default async function handler(req, res) {
 	const { postTitle, postContent, email } = req.body
 
+	const user = await findUserInDatabase(email)
+	const userId = user.id
+    
+
+
 }
 
 
+async function findUserInDatabase(email) {
+	// pobranie użytkownika z bazy danych na podstawie adresu email
+
+	const [users] = await connection.promise().query(`SELECT * FROM userdata`)
+
+	return users.find((user) => user.email === email)
+}
 
 
