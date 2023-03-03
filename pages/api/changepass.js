d03135c512ca896b28e6b81b744a511a84ee2a97
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-03-03 09:52:25 +0100

    receiving data & first functions

---
+++ pages/api/changepass.js
@@ -0,0 +1,25 @@
+import connection from '../../utils/db'
+
+export default async function handler(req, res) {
+	const { email, password, confirmPassword } = req.body
+
+	const user = await findUserInDatabase(email)
+	const userId = user.id
+    
+	const userData = {
+        newPassword: newPassword,
+		user_id: userId,
+	}
+}
+
+async function findUserInDatabase(email) {
+	// pobranie użytkownika z bazy danych na podstawie adresu email
+
+	const [users] = await connection.promise().query(`SELECT * FROM userdata`)
+
+	return users.find((user) => user.email === email)
+}
+
+async function updateUserPassword(userData) {
+
+}
