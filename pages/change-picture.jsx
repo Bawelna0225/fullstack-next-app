df196117ad2be18d4fb347c1520cb608d97f3d4f
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-03-06 12:10:29 +0100

    trying to make cropping work(it doesn't)

--- pages/change-picture.jsx
+++ pages/change-picture.jsx
@@ -1,13 +1,53 @@
 import React, { useState } from 'react'
-import Dropzone from 'react-dropzone'
-import 'react-image-crop/dist/ReactCrop.css'
 import ReactCrop from 'react-image-crop'
+import 'react-image-crop/dist/ReactCrop.css'
 
 export default function changePicture() {
-	const [file, setFile] = useState(null)
-	const [crop, setCrop] = useState({ aspect: 1 / 1 })
-	const [croppedImageUrl, setCroppedImageUrl] = useState('')
-	const [croppedImageFile, setCroppedImageFile] = useState(null)
+	const [imagePreview, setImagePreview] = useState(null)
+	const [crop, setCrop] = useState({ aspect: 1 })
 
-	return <div></div>
+	function handleFileInputChange(event) {
+		const file = event.target.files[0]
+		const reader = new FileReader()
+		reader.onload = function (event) {
+			setImagePreview(event.target.result)
+		}
+		reader.readAsDataURL(file)
+	}
+
+	function handleImageCropComplete(croppedAreaPixels, croppedImagePixels) {
+		// `croppedImagePixels` is a base64-encoded string of the cropped image
+		// const croppedImageFile = dataURItoFile(croppedImagePixels, 'cropped-image.jpg')
+        console.log('first')
+		// send `croppedImageFile` to the server or perform other necessary actions
+	}
+	function handleClearImage() {
+		setImagePreview(null)
+	}
+
+	// function dataURItoFile(dataURI, filename) {
+	// 	const byteString = atob(dataURI.split(',')[1])
+	// 	const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+	// 	const ab = new ArrayBuffer(byteString.length)
+	// 	const ia = new Uint8Array(ab)
+	// 	for (let i = 0; i < byteString.length; i++) {
+	// 		ia[i] = byteString.charCodeAt(i)
+	// 	}
+	// 	return new File([ab], filename, { type: mimeString })
+	// }
+
+	return (
+		<div>
+			{/* <img src={imagePreview} alt="" /> */}
+			<input type="file" onChange={handleFileInputChange} />
+			{imagePreview && (
+				<div style={{ width: '500px', height: '500px', outline: '1px' }}>
+					<ReactCrop crop={crop} onComplete={handleImageCropComplete}>
+						<img src={imagePreview} />
+					</ReactCrop>
+				</div>
+			)}
+			<button onClick={handleClearImage}>Clear image</button>
+		</div>
+	)
 }
