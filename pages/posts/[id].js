9f1d7b76f5f4eeb62a21cc90d6c1775bffcff86d
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-03-09 11:08:35 +0100

    navbar in post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,8 @@
 import React from 'react'
 import connection from '../../utils/db'
+import Router from 'next/router'
+import Navbar from '@/components/Navbar'
+import { useSession } from 'next-auth/react'
 
 export const getStaticPaths = async () => {
 	const [usersPosts] = await connection.promise().query('SELECT * FROM userposts ORDER BY post_id desc')
@@ -20,18 +23,28 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async (context) => {
 	const id = context.params.id
 	const [usersPosts] = await connection.promise().query(`SELECT * FROM userposts WHERE post_id = ${id}`)
-	const posts = JSON.parse(JSON.stringify(usersPosts))
+	const [postsAuthors] = await connection.promise().query(`SELECT * FROM userdata`)
 
+	const posts = JSON.parse(JSON.stringify(usersPosts))
+	const authors = JSON.parse(JSON.stringify(postsAuthors))
 	return {
 		props: {
 			posts,
+			authors,
 		},
 	}
 }
-export default function Post({ posts }) {
-    console.log();
-	return <div>
-        <h1>{posts[0].title}</h1>
-        <pre>{posts[0].content}</pre>
-    </div>
+export default function Post({ posts, authors }) {
+	const { status, data } = useSession()
+
+	const user = authors.filter((author) => author.email === data?.user.email)
+	return (
+		<>
+			<Navbar userData={user} />
+			<div>
+				<h1>{posts[0].title}</h1>
+				<pre>{posts[0].content}</pre>
+			</div>
+		</>
+	)
 }
