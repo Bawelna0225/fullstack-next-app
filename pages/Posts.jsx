b7b1ca2d83044694712ab1e413a7bed1ea6ca6b2
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-03-15 10:53:35 +0100

    animating loader in

--- pages/Posts.jsx
+++ pages/Posts.jsx
@@ -16,14 +16,14 @@ const Posts = ({ posts, commentsQuantity, authors }) => {
 					<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 38 38">
 						<defs>
 							<linearGradient x1="8.042%" y1="0%" x2="65.682%" y2="23.865%" id="a">
-								<stop stop-color="#fff" stop-opacity="0" offset="0%" />
-								<stop stop-color="#fff" stop-opacity=".631" offset="63.146%" />
-								<stop stop-color="#fff" offset="100%" />
+								<stop stopColor="#fff" stopOpacity="0" offset="0%" />
+								<stop stopColor="#fff" stopOpacity=".631" offset="63.146%" />
+								<stop stopColor="#fff" offset="100%" />
 							</linearGradient>
 						</defs>
-						<g fill="none" fill-rule="evenodd">
+						<g fill="none" fillRule="evenodd">
 							<g transform="translate(1 1)">
-								<path d="M36 18c0-9.94-8.06-18-18-18" id="Oval-2" stroke="url(#a)" stroke-width="2">
+								<path d="M36 18c0-9.94-8.06-18-18-18" id="Oval-2" stroke="url(#a)" strokeWidth="2">
 									<animateTransform attributeName="transform" type="rotate" from="0 18 18" to="360 18 18" dur="0.9s" repeatCount="indefinite" />
 								</path>
 								<circle fill="#fff" cx="36" cy="18" r="1">
