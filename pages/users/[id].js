2a76c367dd193f44f8bf63848843a5aaa94b0371
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-03-16 10:38:59 +0100

    static props, fetching data from db

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -2,6 +2,8 @@ import React from 'react'
 import connection from '../../utils/db'
 import Navbar from '@/components/Navbar'
 import { useSession } from 'next-auth/react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 export const getStaticPaths = async () => {
 	const [usersInfo] = await connection.promise().query('SELECT * FROM userdata')
@@ -19,9 +21,27 @@ export const getStaticPaths = async () => {
 		fallback: false,
 	}
 }
+export const getStaticProps = async (context) => {
+	const id = context.params.id
+	const [selectUser] = await connection.promise().query(`SELECT * FROM userdata WHERE id = ${id}`)
+	const [selectAllUsers] = await connection.promise().query(`SELECT * FROM userdata`)
+	const [userPosts] = await connection.promise().query(`SELECT * FROM userposts WHERE author_id = ${id}`)
+	const [userDetails] = await connection.promise().query(`SELECT * FROM userdetails WHERE id = ${id}`)
 
-export default function UserPage() {
-  return (
-    <div>UserPage</div>
-  )
+	const user = JSON.parse(JSON.stringify(selectUser))
+	const allUsers = JSON.parse(JSON.stringify(selectAllUsers))
+	const posts = JSON.parse(JSON.stringify(userPosts))
+	const details = JSON.parse(JSON.stringify(userDetails))
+	return {
+		props: {
+			user,
+			allUsers,
+			posts,
+			details,
+			id,
+		},
+	}
+}
+export default function UserPage({ user, allUsers, posts, details, id }) {
+	return <></>
 }
