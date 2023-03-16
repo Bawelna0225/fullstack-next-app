0418d3e76eaba8f8edc69794f0dc26342a0b7b08
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-03-16 10:21:26 +0100

    get static paths

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -3,6 +3,23 @@ import connection from '../../utils/db'
 import Navbar from '@/components/Navbar'
 import { useSession } from 'next-auth/react'
 
+export const getStaticPaths = async () => {
+	const [usersInfo] = await connection.promise().query('SELECT * FROM userdata')
+	const users = JSON.parse(JSON.stringify(usersInfo))
+
+	const paths = users.map((user) => {
+		return {
+			params: {
+				id: user.id.toString(),
+			},
+		}
+	})
+	return {
+		paths,
+		fallback: false,
+	}
+}
+
 export default function UserPage() {
   return (
     <div>UserPage</div>
