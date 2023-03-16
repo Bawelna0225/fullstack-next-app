ba35b10cef5a951fcf6090cbbbefe076e237d33a
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-03-16 10:20:38 +0100

    imports

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,4 +1,7 @@
 import React from 'react'
+import connection from '../../utils/db'
+import Navbar from '@/components/Navbar'
+import { useSession } from 'next-auth/react'
 
 export default function UserPage() {
   return (
