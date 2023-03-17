ec902d7b9fa3259658e9a7849d6c6eefd3cdd75f
Paweł Czarnecki <pawelczarnecki0225@gmail.com>
2023-03-17 13:19:42 +0100

    fetching data based on received post id

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,1 +1,19 @@
+import connection from '../../utils/db'
 
+export default async function handler(req, res) {
+	const id = req.body
+
+    const selectComments = await selectPostComments(id)
+	res.status(200).json({ selectComments })
+
+	async function selectPostComments(id) {
+		const sql = `SELECT * FROM postcomments WHERE post_id = ${id} ORDER BY comment_id desc`
+		try {
+			const [result] = await connection.promise().execute(sql)
+	        return result
+		} catch (error) {
+			console.error(`Error fetching comments: ${error}`)
+			return null
+		}
+	}
+}
